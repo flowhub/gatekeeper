607351c7097aa9c2236a66acf366de42aaa6b443
Jon Nordby <jononor@gmail.com>
2021-01-02 19:58:39 +0100

    tests: Basic happy and sad cases
    Looks to pass with Node.js v15.1.0

--- spec/api.js
+++ spec/api.js
@@ -1,10 +1,11 @@
 
 const server = require('../server');
+const config = require('../config.json');
 
 const chai = require('chai');
 const nock = require('nock');
 
-//const fetch = require('node-fetch');
+const fetch = require('node-fetch');
 
 const startServer = (port) => {
   return new Promise((resolve, reject) => {
@@ -19,15 +20,27 @@ const startServer = (port) => {
   })
 }
 
-// TODO: implement mocking of Github API
-// https://github.com/prose/gatekeeper/blob/development/tests/server.test.js
-// has some tests now. Using nock, supertest, tape
-// nock is fine, can reuse those.
-// Want to keep mocha and chai for the rest
+const mockGithubAccessTokenValid = (client, returnToken='mockedToke') => {
+
+  const clientConfig = config.clients[client];
+
+  const host = config.oauth_host;
+  const mockUrl = `https://${host}`;
+
+  return nock(mockUrl)
+    .post(config.oauth_path, () => true)
+    .reply(200, {
+        access_token: returnToken,
+        token_type: 'bearer',
+    });
+}
+
 
 describe('OAuth token exchange', () => {
   var port = 3335
   var server = null
+
+  const authenticateApi = "http://localhost:"+port+'/authenticate';
 
   before(() =>
     startServer(port).then((app) => {
@@ -38,18 +51,49 @@ describe('OAuth token exchange', () => {
     return Promise.resolve(null);
   })
 
-  describe('unknown client', () => {
+  afterEach(() => {
+    nock.cleanAll();
+  });
 
-    it('should fail', (done) => {
-        chai.expect(null).to.equal(1);
-        return done(null)
+  describe('unknown client', () => {
+    it('should give 404', () => {
+        const code = 'irrelevant';
+        return fetch(authenticateApi+'/unknown_client/'+code).then((res) => {
+            chai.expect(res.status).to.equal(404);
+        })
     })
   })
 
-  /*     it 'invalid OAuth code should fail', (done) ->
+  // No mocking, hits real Github API
+  describe('valid client with invalid code', () => {
+    it('should give 402', () => {
+        const code = 'invalid';
+        return fetch(authenticateApi+'/default/'+code).then((res) => {
+            chai.expect(res.status).to.equal(402);
+        })
+    })
+  })
 
- */
+  describe('valid client with valid code', () => {
+    var mock = null;
+    before(() => {
+      mock = mockGithubAccessTokenValid('default', 'returnedToken');
+    })
+
+    it('should give 200', () => {
+        const code = 'valid';
+        return fetch(authenticateApi+'/default/'+code).then((res) => {
+            chai.expect(res.status).to.equal(200);
+            chai.expect(mock.isDone()).to.be.true;
+            return res;
+        })
+        .then(res => res.json())
+        .then(json => {
+            chai.expect(json.token).to.be.a('string');
+            chai.expect(json.token).to.equal('returnedToken');
+        })
+    })
+  })
+
+
 })
-
-
-
